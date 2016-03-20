fd3502dc7d1c5b705a3cbeed7bb571a4d5de0f12
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-03-20 15:03:33 -0500

    added odettejs folder for review according to this tutorial http://www.erikschierboom.com/2015/11/07/building-a-javascript-library-part-7-cdn/

---
+++ ajax/libs/odettejs/package.js
@@ -0,0 +1,9 @@
+{
+    "filename": "odette.min.js",
+    "name": "odettejs",
+    "version": "0.0.0",
+    "description": "Odette.js",
+    "keywords": ["odette"],
+    "homepage": "https://github.com/odette-js/odette-js.github.io",
+    "dependencies": {}
+}
