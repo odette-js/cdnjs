0301211b42e20db4d38253c7293a260092d8ea68
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-03-20 15:20:00 -0500

    fixed according to contribute.md. fixed failed tests

--- ajax/libs/odettejs/package.js
+++
@@ -1,9 +0,0 @@
-{
-    "filename": "odette.min.js",
-    "name": "odettejs",
-    "version": "0.0.0",
-    "description": "Odette.js",
-    "keywords": ["odette"],
-    "homepage": "https://github.com/odette-js/odette-js.github.io",
-    "dependencies": {}
-}
